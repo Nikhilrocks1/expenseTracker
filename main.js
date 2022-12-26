65f5c41763ec28a26c466c257eba66c5ec2d7a1d
Govind Patidar <govindpatidar397@gmail.com>
2022-12-26 07:51:19 -0800

    promises

--- main.js
+++ main.js
@@ -1,54 +1,112 @@
-
-class Student{
-    static count=0;
-    
-    constructor(name,age,phone,marks){
-    
-    this.name=name;
-    
-    this.age=age;
-    
-    this.phone=phone;
-    
-    this.marks=marks;
-    
-    Student.count_Students();    
-    }
+/*
+Posts=[
+      {title:'Post one' , body:'this is post one',createdAt:new Date().getTime()},
+      {title:'Post two', body:'this is post two',createdAt:new Date().getTime()}
+     ]
    
-    static count_Students(){
-        return Student.count++;
-    }
-   eligible(com_marks){
-       return (com_age)=>{
-             if(this.marks>com_marks && this.age>com_age){
-                console.log(true);
-                console.log(this.name);
-             }
-             else{
-                console.log(false);
-             }
-       }
-   }
- }
-    
-    
-    
-    const ram=new Student("ram",20,1234,75);
-    
-    const shyam=new Student("shyam",35,1234,80);
-    
-    const balram=new Student("balram",19,1234,35);
-    
-    const bharat=new Student("bharat",27,1234,65);
-    
-    const laxman=new Student("laxman",10,1234,38);
-    
-    console.log(Student.count_Students());
-      
-  ram.eligible(70)(21);
-  shyam.eligible(70)(21);
-  balram.eligible(70)(21);
-  bharat.eligible(70)(21);
-  laxman.eligible(70)(21);
+     let intervalId;
+     function getPosts(){
+          clearInterval(intervalId);
+        intervalId= setTimeout(() => {
+            let output='';
+            Posts.forEach(post => {
+                output+=`<li>${post.title} -last seen ${post.createdAt} </li>`
+            });
+            
+            document.body.innerHTML=output;
+         }, 1000);
+     }
 
-      
+     
+
+     const createPost=(post)=>{
+        return new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                Posts.push({...post,createdAt:new Date().getTime()});
+            const error=false;
+            if(!error){
+                resolve();
+            }
+            else{
+               reject('Error:something went wrong');
+            }
+             }, 2000);
+        })
+     }
+    */
+    function deletePost(){
+        return new Promise((resolve,reject)=>{
+           setTimeout(() => {
+            if(Posts.length>0){
+                Posts.pop();
+                resolve(Posts);
+            }
+            else{
+                reject('error:posts is empty');
+            }
+           },1000);
+        })
+     }
+
+  /*  createPost({title:'Post three', body:'this is post three'})
+    .then(()=>{
+        getPosts();
+        deletePost().then(()=>{
+            getPosts();
+            deletePost().then(()=>{
+                    getPosts();
+                    deletePost().then(()=>{
+                        getPosts();
+                        deletePost().then(()=>{console.log('deleted')})
+                        .catch(()=>{
+                             createPost({title:'Post four',body:'this is post four'})
+                             .then(()=>{
+                                getPosts();
+                                deletePost().then(()=>{
+                                    getPosts();
+                                    deletePost().then(()=>{})
+                                    .catch((error)=>{console.log(error)});
+                                })
+                             })
+                        })
+                    })
+            })
+        })
+    })
+    .catch(err=>console.log(err)); */
+  
+Posts=[];
+
+    const user={
+        username:'Nikhil',
+        lastActivityTime:new Date()
+    }
+    const user1={
+        username:'Neeraj',
+        lastActivityTime:new Date()
+    }
+
+    const createPost=(user)=>{
+        return new Promise((resolve,reject)=>{
+          setTimeout(() => {
+            resolve(Posts.push(user));
+            reject();
+          }, 1000);
+        })
+    }
+
+    const updateLastUserActivityTime=(user)=>{
+        return new Promise((resolve,reject)=>{
+            setTimeout(() => {
+             user.lastActivityTime=new Date();
+            resolve(user.lastActivityTime);
+            }, 1000);
+        })
+    }
+
+    Promise.all([createPost(user),createPost(user1),updateLastUserActivityTime(user),updateLastUserActivityTime(user1)]).then((values)=>{
+        deletePost().then((values)=>{
+          console.log(values);
+        })
+    }
+  );
