2a4786e6f2cb3417ae9efb06a5cf431c5d070dad
Govind Patidar <govindpatidar397@gmail.com>
2023-01-18 01:39:33 -0800

    creteserver2nd

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log('Govind Patidar');
+    console.log(req.url ,req.method, req.headers);
+   
+    switch(req.url){
+        case '/':
+            console.log('hii govind patidar');
+            break;
+        case '/about':
+            console.log('welcome to about us page');
+            break;
+        case '/home':
+            console.log('welcome home');
+            break;
+        case '/node':
+            console.log('welcome to node js project');
+            break;
+      
+    }
+    
 })
 
 server.listen(4000);
