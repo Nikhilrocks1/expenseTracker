1de44b049debfd9638c22b8fadcf0a7fd2dd4b32
Govind Patidar <govindpatidar397@gmail.com>
2023-01-23 03:05:27 -0800

    middleware

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const http=require('http');
 
-const routes=require('./routes');
-const server=http.createServer(routes);
-server.listen(3000);
+const express=require('express');
+const app=express();
+
+app.use((req,res,next)=>{
+    console.log('inside first middleware');
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log('inside another middleware');
+});
+
+
+app.listen(3000);
+
