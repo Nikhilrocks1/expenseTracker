9302b037f7012e6800a07c8305890d964fa1e82f
Govind Patidar <govindpatidar397@gmail.com>
2023-01-23 17:48:08 -0800

    routing via express middleware

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const http=require('http');
-
+const bodyParser=require('body-parser');
 const express=require('express');
-const app=express();
 
-app.use((req,res,next)=>{
-    console.log('inside first middleware');
-    next();
+const app=express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="productName"><input type="number" name="size"><button type="submit">add product</button></form>');
 });
 
-app.use((req,res,next)=>{
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/");
+});
+
+app.use('/',(req,res,next)=>{
     console.log('inside another middleware');
+    res.send('<h1>Hello from express</h1>');
 });
 
 
