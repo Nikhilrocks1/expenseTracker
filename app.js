27461a1655c9fdbf8250ce6c943489c824ec0ec7
Govind Patidar <govindpatidar397@gmail.com>
2023-01-27 12:41:48 -0800

    routers&filters

--- app.js
+++ app.js
@@ -3,21 +3,18 @@ const bodyParser=require('body-parser');
 const express=require('express');
 
 const app=express();
+
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="productName"><input type="number" name="size"><button type="submit">add product</button></form>');
-});
 
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect("/");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/',(req,res,next)=>{
-    console.log('inside another middleware');
-    res.send('<h1>Hello from express</h1>');
-});
-
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>error:page not found</h1>');
+})
 
 app.listen(3000);
 
