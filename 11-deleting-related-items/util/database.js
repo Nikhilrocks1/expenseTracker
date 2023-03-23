18cc243c82b8ab7e3e11a6eca5301d9645c38bc8
Govind Patidar <govindpatidar397@gmail.com>
2023-03-22 21:33:55 -0700

    ONETOONENMANY relationship

--- 11-deleting-related-items/util/database.js
+++ 11-deleting-related-items/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'Abcd@1234', {
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
   dialect: 'mysql',
   host: 'localhost'
 });
